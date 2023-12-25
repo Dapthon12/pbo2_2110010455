26d022dd2ef90792051ec2892c501fa8f39f4806
Dapthon12 <daudalpathon@gmail.com>
2023-12-25 13:57:09 +0800

    perubahan database sql

--- pbo2_2110010455.sql
+++ pbo2_2110010455.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2023 at 12:24 AM
+-- Generation Time: Dec 25, 2023 at 06:55 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
